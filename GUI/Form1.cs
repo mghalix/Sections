488bc562978f1202eaea788591f66f4800b2c7f8
Mohanad <74559094+mghalix@users.noreply.github.com>
2021-12-22 06:51:32 +0200

    Add files via upload

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GUI_Section
+namespace GUIPractice
 {
     public partial class Form1 : Form
     {
@@ -20,66 +20,34 @@ namespace GUI_Section
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string name = textBox1.Text;
-            int age = int.Parse(textBox2.Text);
-            DialogResult msgBox = MessageBox.Show("Are you sure?", "Alert", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
-            if (msgBox == DialogResult.No)
-            {
-                return;
-            }
-            else if (msgBox == DialogResult.Cancel)
-            {
-                return;
-            }
-
-            string display = $"Name: {name}\r\nAge: {age}";
-            if (radioButton1.Checked)
-                display += $"\r\nGender: {radioButton1.Text}";
-            else if (radioButton2.Checked)
-                display += $"\r\nGender: {radioButton2.Text}";
-            else
-            {
-                MessageBox.Show($"Please select your {groupBox1.Text}", "Incomplete", MessageBoxButtons.OKCancel,MessageBoxIcon.Information);
-                return;
-            }
-
-            if (ABp.Checked)
-                display += $"\r\nBlood Type: {ABp.Text}";
-            else if (ABn.Checked)
-                display += $"\r\nBlood Type: {ABn.Text}";
-            else if (Ap.Checked)
-                display += $"\r\nBlood Type: {Ap.Text}";
-            else if (An.Checked)
-                display += $"\r\nBlood Type: {An.Text}";
-            else if (Bp.Checked)
-                display += $"\r\nBlood Type: {Bp.Text}";
-            else if (Bn.Checked)
-                display += $"\r\nBlood Type: {Bn.Text}";
-            else if (Op.Checked)
-                display += $"\r\nBlood Type: {Op.Text}";
-            else if(On.Checked)
-                display += $"\r\nBlood Type: {On.Text}";
-            else
-            {
-                MessageBox.Show($"Please Select Your {groupBox2.Text}", "Incomplete", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-                return;
-            }
-
-
-            textBox3.Text = display;
-
+            int n1 = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            int sum = n1 + n2;
+            label3.Text = sum.ToString();
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            DialogResult x = MessageBox.Show("Are you sure you want to exit?", "Alert", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-            if (x == DialogResult.Yes)
-            {
-                e.Cancel = !true;
-            }
-            else
-                e.Cancel = true;
+            int n1 = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            int subs = n1 - n2;
+            label3.Text = subs.ToString();
         }
-       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            int multi = n1 * n2;
+            label3.Text = multi.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            int divi = n1 / n2;
+            label3.Text = divi.ToString();
+        }
     }
 }
